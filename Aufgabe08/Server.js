7876a41567a8cd0a052782d5c118bc84424655dc
Lukas Willmann <LukasWillmann@gmx.de>
2020-06-16 18:50:13 +0200

    änderung?...

--- Aufgabe08/Server.js
+++ Aufgabe08/Server.js
@@ -20,7 +20,7 @@ var A08Server;
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url + "Was geht du Pisser?");
+        _response.write(_request.url);
         _response.end();
     }
 })(A08Server = exports.A08Server || (exports.A08Server = {}));
--- Aufgabe08/Server.ts
+++ Aufgabe08/Server.ts
@@ -17,7 +17,7 @@ export namespace A08Server {
     console.log("I hear voices!");
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
-    _response.write(_request.url + "Was geht du Pisser?");
+    _response.write(_request.url);
     _response.end();
   }
 }
